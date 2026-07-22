555954d40006b0b82c4d6165515571dbe3748eb2
dealamazonb <dealamazon.b@gmail.com>
2026-07-22 17:55:50 +0200

    Delete src/videoDuration.js

--- src/videoDuration.js
+++
@@ -1,7 +0,0 @@
-// Valeurs par défaut.
-// GitHub Actions remplacera automatiquement ce fichier avant le rendu.
-
-export const VIDEO_FPS = 30;
-export const AUDIO_DURATION_SECONDS = 11;
-export const VIDEO_DURATION_SECONDS = 11.9;
-export const VIDEO_DURATION_IN_FRAMES = 357;
