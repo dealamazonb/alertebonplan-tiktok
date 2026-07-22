ad8711d2263dd3118664e22d2d3825e4224db55e
dealamazonb <dealamazon.b@gmail.com>
2026-07-22 17:54:03 +0200

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {DealVideo} from './DealVideo';
-import {
-  VIDEO_DURATION_IN_FRAMES,
-  VIDEO_FPS,
-} from './videoDuration';
 
 export const RemotionRoot = () => {
   return (
     <Composition
       id="DealVideo"
       component={DealVideo}
-      durationInFrames={VIDEO_DURATION_IN_FRAMES}
-      fps={VIDEO_FPS}
+      // Valeur temporaire remplacée automatiquement par GitHub Actions.
+      durationInFrames={300}
+      fps={30}
       width={1080}
       height={1920}
       defaultProps={{
