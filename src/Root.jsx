46b77f43581dea5b9efc954cff73551d007f153b
dealamazonb <dealamazon.b@gmail.com>
2026-07-22 17:49:06 +0200

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {DealVideo} from './DealVideo';
+import {
+  VIDEO_DURATION_IN_FRAMES,
+  VIDEO_FPS,
+} from './videoDuration';
 
 export const RemotionRoot = () => {
   return (
     <Composition
       id="DealVideo"
       component={DealVideo}
-      durationInFrames={360}
-      fps={30}
+      durationInFrames={VIDEO_DURATION_IN_FRAMES}
+      fps={VIDEO_FPS}
       width={1080}
       height={1920}
       defaultProps={{
