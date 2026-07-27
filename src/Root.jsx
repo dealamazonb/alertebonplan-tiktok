0c7f0ed1833cb723b537a7293c4d7322c72964d6
dealamazonb <dealamazon.b@gmail.com>
2026-07-27 22:59:03 +0200

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {DealVideo} from './DealVideo';
+import DealVideo from './DealVideo';
 import {FPS, TOTAL_FRAMES} from './timings';
 
-export const RemotionRoot = () => (
-  <Composition
-    id="DealVideo"
-    component={DealVideo}
-    durationInFrames={TOTAL_FRAMES}
-    fps={FPS}
-    width={1080}
-    height={1920}
-    defaultProps={{
-      title: 'Bon plan Amazon',
-      shortTitle: '',
-      currentPrice: '',
-      originalPrice: '',
-      discount: '',
-      imageUrl: '',
-      format: 'price_only',
-    }}
-  />
-);
+export const RemotionRoot = () => {
+  return (
+    <Composition
+      id="DealVideo"
+      component={DealVideo}
+      durationInFrames={TOTAL_FRAMES}
+      fps={FPS}
+      width={1080}
+      height={1920}
+      defaultProps={{
+        title: 'Bon plan Amazon',
+        shortTitle: '',
+        currentPrice: '34,09 €',
+        originalPrice: '',
+        discount: '',
+        imageUrl: '',
+        format: 'price_only',
+      }}
+    />
+  );
+};
