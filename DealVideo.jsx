582745f934fa297580bc3e6209b6dfe53a892b31
dealamazonb <dealamazon.b@gmail.com>
2026-07-27 22:29:52 +0200

    Add files via upload

---
+++ DealVideo.jsx
@@ -0,0 +1,8 @@
+export const FPS = 30;
+export const HOOK_FROM = 0;
+export const HOOK_FRAMES = 75;
+export const DEAL_FROM = HOOK_FROM + HOOK_FRAMES;
+export const DEAL_FRAMES = 150;
+export const CTA_FROM = DEAL_FROM + DEAL_FRAMES;
+export const CTA_FRAMES = 60;
+export const TOTAL_FRAMES = CTA_FROM + CTA_FRAMES;
