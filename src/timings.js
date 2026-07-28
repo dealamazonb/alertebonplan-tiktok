dee656817d77b296cace30a051c2326f2e196a65
dealamazonb <dealamazon.b@gmail.com>
2026-07-28 19:52:57 +0200

    Add files via upload

---
+++ src/timings.js
@@ -0,0 +1,12 @@
+export const FPS = 30;
+
+export const HOOK_FROM = 0;
+export const HOOK_FRAMES = 42;
+
+export const DEAL_FROM = HOOK_FROM + HOOK_FRAMES;
+export const DEAL_FRAMES = 170;
+
+export const CTA_FROM = DEAL_FROM + DEAL_FRAMES;
+export const CTA_FRAMES = 58;
+
+export const TOTAL_FRAMES = CTA_FROM + CTA_FRAMES;
